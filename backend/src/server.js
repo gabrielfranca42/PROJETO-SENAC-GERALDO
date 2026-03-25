9ac9610fcc49064d3532d27343941fa4170d1537
BILLY BAT <gabrielfranca172@gmail.com>
2026-03-25 16:14:41 -0300

    erro no server e por algum motivo AINDA NO DOCKER ESSA PORRA

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config(); 
 const app = require('./app');
+const connectDB = require('./config/db'); // 1. Importe o seu ficheiro de conexão
 
-// Se tiver o ficheiro connectDB.js, importe-o e execute-o aqui:
-// const connectDB = require('./config/connectDB');
-// connectDB();
-
+// Teste temporário ignorando o .env
+process.env.MONGO_URI = "mongodb://root:rootpassword@127.0.0.1:27017/pi_db?authSource=admin";
+connectDB();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor a correr com sucesso na porta ${PORT}.`);
+  // Este log agora deve mostrar a string correta, não 'undefined'
+  console.log("URI Carregada:", process.env.MONGO_URI); 
 });
