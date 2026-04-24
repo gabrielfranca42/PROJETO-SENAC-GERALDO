556896a909779524f725470e41147213c0d3074a
BILLY BAT <gabrielfranca172@gmail.com>
2026-04-24 10:17:55 -0300

    ok bug fix ja feito agora se der tempo testes unitarios prisma e jogar pra cluster

--- backend/src/models/Course.js
+++ backend/src/models/Course.js
@@ -3,14 +3,25 @@ const mongoose = require('mongoose');
 const CategoryRuleSchema = new mongoose.Schema({
   name: { type: String, required: true },
   maxHours: { type: Number, required: true }, // Limite total do curso para a categoria
-  semesterMaxHours: { type: Number, required: true } // [NOVO - Requisito UI] Limite semestral da categoria
+  semesterMaxHours: { type: Number, required: true } // Limite semestral da categoria
 });
 
 const CourseSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   totalHoursRequired: { type: Number, required: true },
-  semesterMaxHours: { type: Number, required: true }, // [NOVO - Requisito UI] Limite semestral global do curso
-  categories: [CategoryRuleSchema]
+  semesterMaxHours: { type: Number, required: true }, // Limite semestral global do curso
+  categories: [CategoryRuleSchema],
+  
+  // =========================================================================
+  // Ao exigir um 'ObjectId' referenciando o model 'User',
+  // garantimos a regra "1 Curso tem 1 Coordenador". Para descobrir os "vários
+  // cursos de um coordenador", basta fazer uma query: Course.find({ coordinator: id })
+  // =========================================================================
+  coordinator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Referência exata ao modelo de Usuários
+    required: [true, 'Um curso deve obrigatoriamente ter um coordenador vinculado.']
+  }
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
