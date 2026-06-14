1d362b692cde5c4cd1a37716331ac07037f79376
gabrielfranca42 <gabrielfranca172@gmail.com>
2026-06-14 13:31:33 -0300

    hotifix de uso de rota  JSON Parse error: Unexpected character

--- backend/src/modules/activities/activities.routes.js
+++ backend/src/modules/activities/activities.routes.js
@@ -9,9 +9,16 @@ const authenticate = require('../../middlewares/auth');
 const authorize = require('../../middlewares/authRole');
 
 const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, '../../../../uploads/');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../../../../uploads/'));
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
